d5f3294c6d2e617538e959aea9e9743acd56ef59
o0timez <o0chronoz@gmail.com>
2025-02-03 23:17:20 +0700

    Update aboutMe.js

--- aboutMe.js
+++ aboutMe.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Khi nhấn vào ảnh GIF, tải nội dung từ aboutme.md
     aboutMeGif.addEventListener("click", function () {
-        fetch("Aboutme/aboutme.md")
+        fetch("Aboutme/aboutme.txt")
             .then(response => response.text())
             .then(text => {
                 aboutMeContent.innerHTML = marked.parse(text);
